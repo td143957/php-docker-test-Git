054c181a4ee194a425272feb80a3b2240234c7db
Thierno Maladho Diallo <td143957@fh-muenster.de>
2022-09-21 09:23:34 +0200

    php objekte

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">  
+
+<head>
+ <meta charset ="UTF-8">
+ <meta charset="ISO-8859-1">
+ <title> Flex-box besteht aus Flex-Container und Flex-Items </title>
+ <link href="style.css" rel= "stylesheet" type ="text/css"> 
+</head>
+
+<body>
+
+</body>
+</html>
